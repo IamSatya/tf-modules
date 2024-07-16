4eb3ad2bd6acd5628b26d2d68f6e22b0c7576008
Satyanarayana Panuganti <satyapanuganti@gmail.com>
2024-07-16 18:23:24 +0530

    Create inputs.tf

---
+++ ec2/inputs.tf
@@ -0,0 +1,9 @@
+variable "ami_value"{
+  type            = "string"
+  value           = "ami-0059b7cd9f67d8050"
+}
+
+variable "instance_type_value"{
+  type            = "string"
+  value           = "t3.micro"
+}
