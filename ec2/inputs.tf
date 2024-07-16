d1a420f1f63d6059cc76582b7a8a157302f4037a
Satyanarayana Panuganti <satyapanuganti@gmail.com>
2024-07-16 18:30:49 +0530

    Update inputs.tf

--- ec2/inputs.tf
+++ ec2/inputs.tf
@@ -1,9 +1,9 @@
 variable "ami_value"{
-  type            = "string"
+  type            = string
   default           = "ami-0059b7cd9f67d8050"
 }
 
 variable "instance_type_value"{
-  type            = "string"
+  type            = string
   default           = "t3.micro"
 }
